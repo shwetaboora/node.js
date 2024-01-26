b19563c10848674497e5b64b73e1e714f7d99066
shwetaboora <shwetabooraa@gmail.com>
2024-01-27 01:36:23 +0530

    async patterns

--- 2-async-patterns/1-block.js
+++ 2-async-patterns/1-block.js
@@ -3,7 +3,8 @@ const http = require('http')
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
         res.end('Home Page')
-    } else if (req.url === '/about') {
+    }
+    if (req.url === '/about') {
         // blocking code
         for (let i = 0; i < 1000; i++) {
             for (let j = 0; j < 1000; j++) {
@@ -11,11 +12,10 @@ const server = http.createServer((req, res) => {
             }
         }
         res.end('About Page')
-    } else {
-        res.end('Error Page')
     }
+    res.end('Error Page')
 })
 
 server.listen(5000, () => {
     console.log('Server listening on port : 5000....')
-}) 
+})
