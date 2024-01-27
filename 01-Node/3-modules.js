82625ccbb33aad064c5c045587bc766f59def5cc
shwetaboora <shwetabooraa@gmail.com>
2024-01-28 03:38:49 +0530

    rearranged files and also added streams in node

--- 01-Node/3-modules.js
+++ 01-Node/3-modules.js
@@ -1,14 +1,14 @@
 //commonJS, every file is module(by default)
 // Modules - Encapsulated Code (only share minimum)
 
-const names = require('./4-names');
-const sayHi = require('./5-utils');
+const names = require('../4-names');
+const sayHi = require('../5-utils');
 // console.log(names);
 
-const data = require('./6-alternative-flavor');
+const data = require('../6-alternative-flavor');
 // console.log(data);
 
-require('./7-mind-grenade')
+require('../7-mind-grenade')
 sayHi('susan')
 sayHi(names.jhon)
 sayHi(names.sohn)
