9547ef5156d44e888d1c266c642935d48929cd79
shwetaboora <shwetabooraa@gmail.com>
2024-01-27 21:37:13 +0530

    event emitter and request event files

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
     if (req.url === '/') {
         res.end('Home Page')
     } else if (req.url === '/about') {
-        // blocking code
+        // blocking code 
         for (let i = 0; i < 1000; i++) {
             for (let j = 0; j < 1000; j++) {
                 console.log(`${i} ${j}`)
