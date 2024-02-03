17285513a7ff5ebfa18e0bb539eae16e8b108fe6
shwetaboora <shwetabooraa@gmail.com>
2024-02-03 11:23:11 +0530

    express basics and express app eg.

--- 02-Express/files/04-express-app.js
+++ 02-Express/files/04-express-app.js
@@ -4,10 +4,10 @@ const path = require('path')
 const app = express()
 
 // setup static and middleware
-app.use(express.static('./public'))
+app.use(express.static('../public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+    res.sendFile(path.resolve(__dirname, '../navbar-app/index.html'))
 })
 
 app.all('*', (req, res) => {
