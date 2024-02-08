813f8574b8d8403cfe5cd52c5c910ee0529e0ac4
shwetaboora <shwetabooraa@gmail.com>
2024-02-09 04:01:28 +0530

    middleware function's file

--- 02-Express/files/07-params-query.js
+++ 02-Express/files/07-params-query.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { products } = require('./data')
+const { products } = require('../data')
 
 app.get('/', (req, res) => {
     res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
